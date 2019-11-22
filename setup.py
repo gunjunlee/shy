40ebafc4a3a28f4ecbd451910a02ffc0ef547e49
tanukee <tanukee@hpcnt.com>
2019-11-22 14:37:02 +0900

    numpy 2 pillow & add arg: normalized to draw_bbow/draw_point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='shy',
-  version='0.2.4.dev2019111900',
+  version='0.2.4.dev2019112200',
   description='very shy library',
   author='kevin970401',
   author_email='kevin970401@gmail.com',
